9b0fbdff116390dcf9a5c6cffe3ed340d42d256b
Ray <ddarjae@naver.com>
2021-01-11 18:16:05 +0900

    404페이지 오류 수정시도2

---
+++ _site/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"Yes! Swift!  Swift는 일이고 CS(Computer Science)가 취미인 개발자의 블로그   YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다.   이 글의 제목 : github.io 블로그 시작하기  마지막으로 수정된 시간 : 2020-01-11 08:06:00 -0500  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "https://torch-ray.github.io/blog/first-post/",
+        "teaser": null
+      }]
